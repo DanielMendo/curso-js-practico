40861376860aec23a41967de06693148a4c38f2f
Daniel Mendoza <danielmendohernan@hotmail.com>
2022-02-09 01:37:01 -0600

    Se avanzo en la parte de la figura del triangulo

--- figuras.js
+++ figuras.js
@@ -2,43 +2,69 @@
 // console.group('Cuadrado');
 // const ladoCuadrado = 5;
 
-// console.log(`Los lados del cuadrado son: ${ladoCuadrado}cm`);
-
+// Funciones del cuadrado
 function perimetroCuadrado(lado) {
     return lado * 4;
 }
-
-
-// console.log(`El perímetro del cuadrado es: ${perimetroCuadrado}cm`);
 
 function areaCuadrado(lado) {
     return lado * lado;
 }
 
-// console.log(`El área del cuadrado es: ${areaCuadrado}cm2`);
-console.groupEnd()
+// // console.log(`El área del cuadrado es: ${areaCuadrado}cm2`);
+// console.groupEnd()
 
 // Código del triángulo
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
+// const alturaTriangulo = 5.5
 
-console.log(`Los lados del triángulo son: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm, ${baseTriangulo}cm`);
+// console.log(`Los lados del triángulo son: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm, ${baseTriangulo}cm`);
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log(`El perímetro del triángulo es: ${perimetroTriangulo}`);
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
 
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log(`El área del triángulo es de: ${areaTriangulo}`)
+
+// const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
 
 
 
 // Interactuar con HTML
+const valorMedida = document.getElementById("enterMedida")
+const mostrarValorMedida = document.getElementById("mostrarMedida")
+
+
 
 function mostrarResultado(resultado) {
     const inputResultado = document.getElementById("mostrarResultado")
     inputResultado.value = resultado;
+}
+
+function mostrarMedidaArea(medida) {
+    mostrarValorMedida.value = medida;
+
+    if (medida === "cm") {
+        mostrarValorMedida.value = "cm²"
+    } else {
+        mostrarValorMedida.value = "m²"
+    }
+}
+
+function mostrarMedidaPerimetro(medida) {
+    mostrarValorMedida.value = medida;
+}
+
+
+
+/* Funciones cuadrado */
+function calcularAreaCuadrado() {
+    const inputCuadrado = document.getElementById("inputCuadrado");
+    const value = inputCuadrado.value;
+    mostrarResultado(areaCuadrado(value))
+    mostrarMedidaArea(valorMedida.value)
 }
 
 function calcularPerimetroCuadrado() {
@@ -46,11 +72,31 @@ function calcularPerimetroCuadrado() {
     const value = inputCuadrado.value;
 
     mostrarResultado(perimetroCuadrado(value))
+    mostrarMedidaPerimetro(valorMedida.value)
 }
 
-function calcularAreaCuadrado() {
-    const inputCuadrado = document.getElementById("inputCuadrado");
-    const value = inputCuadrado.value;
 
-    mostrarResultado(areaCuadrado(value))
+
+/* Funciones triángulo */
+function calcularPerimetroTriangulo() {
+    const altura = document.getElementById("inputTrianguloAltura")
+    const base = document.getElementById("inputTrianguloBase")
+    const ladoA = document.getElementById("inputTrianguloLadoA")
+    const ladoB = document.getElementById("inputTrianguloLadoB")
+
+    const h = parseInt(altura.valuie);
+    const b = parseInt(base.value);
+    const lado1 = parseInt(ladoA.value);
+    const lado2 = parseInt(ladoB.value)
+
+        
+    let perimetro = perimetroTriangulo(lado1, lado2, b)
+
+    function mostrarResultado(resultado) {
+        const inputResultado = document.getElementById("mostrarResultadoT")
+        inputResultado.value = resultado;
+    }
+    
+    mostrarResultado(perimetro);
 }
+
